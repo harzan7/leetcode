8c83380b166427391ea9cf1bd5641d8261517dee
Harza <harzan12345@gmail.com>
2025-06-19 19:08:31 -0400

    Feat: Implement optimal implementation with two hashmap passes.

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -13,3 +13,27 @@ def two_sum_naive(nums, target):
         for j in range(i + 1, length):
             if nums[i] + nums[j] == target:
                 return [i, j]
+
+
+def two_sum_two_hash(nums, target):
+    """
+    Given an array of integers, return indices of the two numbers such that they add up to target.
+    Involves two passes with hashmaps.
+    Faster solution than naive implementation.
+    O(n) time complexity.
+    O(n) space complexity.
+    :param nums: Array of integers.
+    :param target: Target number.
+    :return: Indices of the two numbers such that they add up to target.
+    """
+    length = len(nums)
+    nums_hash = {}
+
+    for i in range(length):
+        nums_hash[nums[i]] = i
+
+    for i in range(length):
+        complement = target - nums[i]
+        if complement in nums_hash and nums_hash[complement] != i:
+            return [i, nums_hash[complement]]
+
