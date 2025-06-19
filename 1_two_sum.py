ab2a0123100cf6596e1d4869d51664d508711cf5
Harza <harzan12345@gmail.com>
2025-06-19 18:56:15 -0400

    Add docstring to naive implementation.

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,5 +1,13 @@
 
+
 def two_sum_naive(nums, target):
+    """
+    Given an array of integers, return indices of the two numbers such that they add up to target.
+    Naive solution to two-sum problem, involving nested for loops.
+    :param nums: Array of integers.
+    :param target: Target number.
+    :return: Indices of the two numbers such that they add up to target.
+    """
     length = len(nums)
     for i in range(length):
         for j in range(i + 1, length):
