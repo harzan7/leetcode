a72d717b9c766391bef30634c73551d014fa1176
Harza <harzan12345@gmail.com>
2025-07-01 23:27:04 -0400

    Feat 3: Implement optimal solution to problem.

--- 3_longest_substring_without_repeating_characters.py
+++ 3_longest_substring_without_repeating_characters.py
@@ -25,23 +25,46 @@ def length_of_longest_substring_naive(s):
     return max(lengths)
 
 
+def length_of_longest_substring_optimal(s):
+    """
+    Optimal solution to the longest substring without repeating characters' problem.
+    O(n) time complexity.
+    O(min(n, m)) space complexity, where m is the number of unique characters in s.
+    :param s: Given string.
+    :return: Length of the longest substring without repeating characters.
+    """
+    char_set = set()    # unique chars
+    left = 0
+    max_len = 0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+
+        char_set.add(s[right])
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
+
 def main():
     # Tests
     s1 = "abcabcbb"
     expected1 = 3
-    actual1 = length_of_longest_substring_naive(s1)
+    actual1 = length_of_longest_substring_optimal(s1)
     print("Length of longest substring in", s1, "->", actual1)
     assert actual1 == expected1
 
     s2 = "bbbbb"
     expected2 = 1
-    actual2 = length_of_longest_substring_naive(s2)
+    actual2 = length_of_longest_substring_optimal(s2)
     print("Length of longest substring in", s2, "->", actual2)
     assert actual2 == expected2
 
     s3 = "pwwkew"
     expected3 = 3
-    actual3 = length_of_longest_substring_naive(s3)
+    actual3 = length_of_longest_substring_optimal(s3)
     print("Length of longest substring in", s3, "->", actual3)
     assert actual3 == expected3
 
